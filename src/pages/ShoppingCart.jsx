9f4549639ae8b5c64b2d5f327624510a7bd0cef2
Baris <barisbalcimusic@gmail.com>
2024-05-13 09:59:01 +0200

    shopping cart gestyled

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,33 +1,32 @@
-import styled from "styled-components";
 import ProductInCart from "../components/product/ProductInCart";
 import { useCartContext } from "../contexts/CartContext";
-
-const TABLE = styled.table`
-  border: solid black 1px;
-`;
 
 const ShoppingCart = () => {
   const { cart } = useCartContext();
 
   return (
-    <>
+    <div className="shopping-cart">
       <h1>Shopping Cart</h1>
-      <TABLE className="shopping-cart">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((product) => (
-            <ProductInCart key={product.id} product={product} />
-          ))}
-        </tbody>
-      </TABLE>
-      <button>Order</button>
-    </>
+      {cart.length > 1 ? (
+        <table className="shopping-cart-table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((product) => (
+              <ProductInCart key={product.id} product={product} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
+      {cart.length > 0 && <button>Order</button>}
+    </div>
   );
 };
 
