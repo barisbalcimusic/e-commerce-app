8168c442386089237e4d9d2ab8b8c4607817cb8e
Baris <barisbalcimusic@gmail.com>
2024-05-14 09:53:38 +0200

    cart with discounted price updated

--- src/components/product/ProductInCart.jsx
+++ src/components/product/ProductInCart.jsx
@@ -4,7 +4,18 @@ const ProductInCart = ({ product }) => {
   return (
     <tr>
       <td className="title-td">{product.title}</td>
-      <td className="price-td">{product.price} $</td>
+      <td className="price-td">
+        {
+          // if so show discounted price
+          product.discountPercentage > 0
+            ? (
+                product.price -
+                product.price * (product.discountPercentage / 100)
+              ).toFixed(2)
+            : product.price
+        }{" "}
+        $
+      </td>
       <td>
         <Counter product={product} />
       </td>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -18,7 +18,26 @@ const ProductDetail = () => {
       <p className="title">{filteredProduct.title}</p>
       <img src={filteredProduct.thumbnail} alt="product-picture" />
       <p>{filteredProduct.description}</p>
-      <p>{filteredProduct.price} $</p>
+      {/* <p className="price">{filteredProduct.price} $</p> */}
+      <p
+        className={
+          filteredProduct.discountPercentage > 0 ? "old-price" : "price"
+        }
+      >
+        {filteredProduct.price} ${" "}
+      </p>
+      {filteredProduct.discountPercentage > 0 && (
+        <p className="new-price">
+          {
+            //calculate discounted price
+            (
+              filteredProduct.price -
+              filteredProduct.price * (filteredProduct.discountPercentage / 100)
+            ).toFixed(2)
+          }{" "}
+          $
+        </p>
+      )}
       <AddButton product={filteredProduct} />
     </div>
   ) : (
