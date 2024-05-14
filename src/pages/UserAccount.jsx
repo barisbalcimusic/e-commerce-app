6b59f12ab5166860ce2f5054f152538236d93606
Baris <barisbalcimusic@gmail.com>
2024-05-14 12:56:13 +0200

    sign out added

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -3,7 +3,7 @@ import { useAuthContext } from "../contexts/AuthContext";
 import { useEffect } from "react";
 
 const UserAccount = () => {
-  const { isLoggedIn, loggedUser } = useAuthContext();
+  const { isLoggedIn, setIsLoggedIn, loggedUser } = useAuthContext();
   const navigate = useNavigate();
 
   //if not logged in, redirect to home
@@ -11,11 +11,18 @@ const UserAccount = () => {
     if (!isLoggedIn) {
       navigate("/");
     }
-  });
+  }, [isLoggedIn]);
+
+  const handleClick = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
     <div className="user-account-container">
       <h2>Welcome {loggedUser && loggedUser.firstname}!</h2>
+      <button onClick={handleClick} className="sign-out">
+        Sign out
+      </button>
       <div className="user-settings">
         <div className="user-setting">My Orders</div>
         <div className="user-setting">Login and Security</div>
