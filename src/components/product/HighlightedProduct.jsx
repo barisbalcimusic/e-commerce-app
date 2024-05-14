289a994c7576e336b5f8f44823cb7a5ada1c8433
Baris <barisbalcimusic@gmail.com>
2024-05-14 09:34:42 +0200

    discounted price created

--- src/components/product/HighlightedProduct.jsx
+++ src/components/product/HighlightedProduct.jsx
@@ -5,9 +5,21 @@ const HighlightedProduct = ({ product }) => {
     <Link to={`store/product-detail/${product.id}`} className="highlight">
       <img src={product.thumbnail} />
       <p className="title">{product.title}</p>
-      <p>
-        <b>{product.price} $</b>
+      <p className={product.discountPercentage > 0 ? "old-price" : "price"}>
+        {product.price} $
       </p>
+      {product.discountPercentage > 0 && (
+        <p className="new-price">
+          {
+            //calculate discounted price
+            (
+              product.price -
+              product.price * (product.discountPercentage / 100)
+            ).toFixed(2)
+          }{" "}
+          $
+        </p>
+      )}
     </Link>
   );
 };
