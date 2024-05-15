d46d6c8de59a6f3e71b9332466f4ff1066280b0d
Baris <barisbalcimusic@gmail.com>
2024-05-15 21:42:17 +0200

    favicon added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
